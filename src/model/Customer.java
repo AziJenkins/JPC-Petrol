41c3e6d100aad1d0b712dc2b87601a8be12cb067
Jenkins <azi.jenkins@capgemini.com>
2018-06-12 14:27:46 +0100

    added exceptions and filled most of the vehicle class

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -5,13 +5,24 @@ import java.util.UUID;
 public class Customer {
 
 	private UUID registration;
+
 	private Boolean hasPaid = false;
+	private Boolean willShop;
 	private double shopTime;
 	private double shopSpend;
 	
-	public Customer(UUID registration, double shopTime, double shopSpend) {
+	public Customer(UUID registration, double shopTime, double shopSpend, Boolean willShop) {
 		this.registration = registration;
 		this.shopTime = shopTime;
 		this.shopSpend = shopSpend;
+		this.willShop = willShop;
+	}
+	
+	public UUID getRegistration() {
+		return registration;
+	}
+	
+	public Boolean getHasPaid() {
+		return hasPaid;
 	}
 }
--- src/model/Vehicle.java
+++ src/model/Vehicle.java
@@ -6,6 +6,9 @@ package model;
 import java.util.Random;
 import java.util.UUID;
 
+import exceptions.CustomerCarMismatchException;
+import exceptions.MinGreaterThanMaxException;
+
 /**
  * @author AZJENKIN
  *
@@ -13,34 +16,86 @@ import java.util.UUID;
 public abstract class Vehicle {
 	private final double size;
 	private final int fuelCapacity;
+	private final int shoppingTicks;
+	private final double shoppingSpend;
+	private final double shopProbability;
+	private final double ticksBeforeNoShop;
 	private int currentFuel;
 	private Boolean isFueled = false;
 	private Boolean hasPaid = false;
 	private UUID registration;
 	private int ticksSinceArrival = 0;
 	private Random rand = new Random();
-	
-	
-	public Vehicle(double size, int minCapacity, int maxCapacity) {
+
+	public Vehicle(double size, int minCapacity, int maxCapacity, double shopProbability, int ticksBeforeNoShop,
+			int minShopTicks, int maxShopTicks, double minShopSpend, double maxShopSpend) throws MinGreaterThanMaxException {
 		this.size = size;
 		this.registration = UUID.randomUUID();
 		this.currentFuel = 0;
-		if (maxCapacity - minCapacity <= 0) {
-			this.fuelCapacity = maxCapacity;
+		this.shopProbability = shopProbability;
+		this.ticksBeforeNoShop = ticksBeforeNoShop;		
+		this.fuelCapacity = calculateMaxCapacity(minCapacity, maxCapacity);
+		this.shoppingTicks = calculateShoppingTicks(minShopTicks, maxShopTicks);
+		this.shoppingSpend = calculateShopSpend(minShopSpend, maxShopSpend);
+	}
+
+	private int calculateMaxCapacity(int minCapacity, int maxCapacity) throws MinGreaterThanMaxException {
+		int capacityDifference = maxCapacity - minCapacity;
+		if (capacityDifference < 0)
+			throw new MinGreaterThanMaxException();
+		else if (capacityDifference == 0) 
+			return maxCapacity;
+		else 
+			return rand.nextInt(maxCapacity - minCapacity) + minCapacity;
+	}
+
+	private int calculateShoppingTicks(int minShopTicks, int maxShopTicks) throws MinGreaterThanMaxException {
+		int shopTickDifference = maxShopTicks - minShopTicks;
+		if(shopTickDifference < 0)
+			throw new MinGreaterThanMaxException();
+		else if (shopTickDifference == 0) 
+			return maxShopTicks;
+		else 
+			return rand.nextInt(maxShopTicks - minShopTicks) + minShopTicks;
+		
+	}
+
+	private double calculateShopSpend(double minShopSpend, double maxShopSpend) throws MinGreaterThanMaxException {
+		double shopSpendDifference = maxShopSpend - minShopSpend;
+		if (shopSpendDifference < 0)
+			throw new MinGreaterThanMaxException();
+		else if (shopSpendDifference == 0)
+			return maxShopSpend;
+		else 
+			return (rand.nextDouble() * (maxShopSpend - minShopSpend)) + minShopSpend;
+	}
+
+	public Boolean tryFill(int fuelRate) {
+		if (!isFueled) {
+			currentFuel += fuelRate;
+			return true;
 		} else {
-		this.fuelCapacity = rand.nextInt(maxCapacity - minCapacity) + minCapacity;
+			return false;
+		}
+	}
+
+	public Customer leaveCar() {
+		return new Customer(registration, shoppingTicks, shoppingSpend, decideToShop());
+	}
+	
+	public Boolean decideToShop() {
+		return ticksSinceArrival <= ticksBeforeNoShop;
+	}
+
+	public void reEnterCar(Customer c) throws CustomerCarMismatchException {
+		if (c.getRegistration().equals(registration)) {
+			hasPaid = c.getHasPaid();
+		} else {
+			throw new CustomerCarMismatchException();
 		}
 	}
 	
-	public Boolean fill(int fuelRate) {
-	
-	}
-	
-	public Customer leaveCar() {
-		
-	}
-	
-	public void reEnterCar(Customer c) {
-		
+	public void addTick() {
+		this.ticksSinceArrival ++;
 	}
 }
